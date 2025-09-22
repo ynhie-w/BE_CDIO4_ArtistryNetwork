de928bf1dc0b5ddc8de626464b71128ab334d643
ynhie-w <tranthiyennhi3333hhhh@gmail.com>
2025-09-23 01:33:21 +0700

    Xóa CODE_CDIO4 và CODE_CDIO4.sln

--- CODE_CDIO4/DTOs/ThanhToanDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CODE_CDIO4.DTOs
-{
-    public class ThanhToanDTO
-    {
-        public int Id_DonHang { get; set; }
-        public string PhuongThuc { get; set; } = string.Empty;
-        public string? TrangThai { get; set; } = "Chờ xử lý";
-    }
-}
